379bf00a298a55d5ec520fcebd8bf9082a3adcc7
davidsan-pro <davidsan.pro88@gmail.com>
2022-03-23 01:37:39 +0700

    ubah property class="" mjd className=""

--- src/components/MenuDatabase.js
+++ src/components/MenuDatabase.js
@@ -5,26 +5,26 @@ import { Link } from "react-router-dom";
 const MenuDatabase = () => {
   return (
     <div className="container has-text-centered">
-      <div class="mb-3">
+      <div className="mb-3">
         <Link to="/master_user">
           <Button className="btn-menu fw-bold" variant="primary" size="lg">
             MASTER USER
           </Button>
         </Link>
       </div>
-      <div class="mb-3">
+      <div className="mb-3">
         <Link to="/master_barang">
           <Button className="btn-menu fw-bold" variant="primary" size="lg">
             MASTER BARANG
           </Button>
         </Link>
       </div>
-      <div class="mb-3">
+      <div className="mb-3">
         <Button className="btn-menu fw-bold" variant="primary" size="lg">
           MASTER TOKO
         </Button>
       </div>
-      <div class="mb-3">
+      <div className="mb-3">
         <Button className="btn-menu fw-bold" variant="primary" size="lg">
           MASTER RUTE
         </Button>

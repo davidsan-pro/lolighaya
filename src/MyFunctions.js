d48ed30259cf6d89671ae7d1ee9a8791fe91de54
davidsan-pro <davidsan.pro88@gmail.com>
2022-04-08 01:41:37 +0700

    buat func.pendukung

--- src/MyFunctions.js
+++ src/MyFunctions.js
@@ -1,21 +1,37 @@
 export function getNamaHari(number) {
   number = parseInt(number|0);
   let result = '';
-  if (number == 1) {
+  if (number === 1) {
     result = 'senin';
-  } else if (number == 2) {
+  } else if (number === 2) {
     result = 'selasa';
-  } else if (number == 3) {
+  } else if (number === 3) {
     result = 'rabu';
-  } else if (number == 4) {
+  } else if (number === 4) {
     result = 'kamis';
-  } else if (number == 5) {
+  } else if (number === 5) {
     result = 'jumat';
-  } else if (number == 6) {
+  } else if (number === 6) {
     result = 'sabtu';
-  } else if (number == 7) {
+  } else if (number === 7) {
     result = 'minggu';
   }
   
   return result;
+}
+
+export function ucasefirst(string) {
+  if ( ! string) { 
+    return '';
+  } else {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+}
+
+export function ucase(string) {
+  if ( ! string) { 
+    return '';
+  } else {
+    return string.toUpperCase();
+  }
 }

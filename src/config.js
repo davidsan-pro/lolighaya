5ed394808a87b05126d3d7a37ad19c89c03422d4
davidsan-pro <davidsan.pro88@gmail.com>
2022-04-09 11:14:36 +0700

    ubah base_url mjd lolighaya.davidsan.my.id

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = global.config = {
-  base_url: "http://localhost:8080",
+  // base_url: "http://localhost:8080",
+  base_url: "https://lolighaya-be.davidsan.my.id",
   // other global config variables you wish
 };

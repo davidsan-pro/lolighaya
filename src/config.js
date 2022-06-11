65f20a29ca04ed5a542b234f60a9491d882fc973
david santana <101975183+davidsan-pro@users.noreply.github.com>
2022-06-11 16:32:01 +0700

    Update config.js
    coba ubah base_url mjd local spy ketika testing via postman mengarah k coding di localhost

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = global.config = {
-  // base_url: "http://localhost:8080",
-  base_url: "https://lolighaya-be.davidsan.my.id",
+  base_url: "http://localhost:8080",
+  // base_url: "https://lolighaya-be.davidsan.my.id",
   // other global config variables you wish
 };
